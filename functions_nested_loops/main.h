9281fe0e99db2c7412791a4f945e47456ebdcd08
MicaelVelez <mvrod2314@gmail.com>
2025-10-16 14:25:17 -0400

    Headr

---
+++ functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <wctype.h>
+
+int _putchar(char c);
+
+#endif MAIN_H
