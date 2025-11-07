881908fe6558d8837ccdb62ecb33d1be0fc0ecc6
Micael Velez <mvrod2314@gmail.com>
2025-11-07 18:38:55 +0000

    doge

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,13 +6,13 @@
  * @age: dogs age
  * @owner: pointer to owners name
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-typedef struct dog dog_t;
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
