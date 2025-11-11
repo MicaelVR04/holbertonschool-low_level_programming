afd711c7f814d17f5aa6de8f27078c1be4897eeb
MicaelVelez <mvrod2314@gmail.com>
2025-11-11 10:35:21 -0400

    vrdcfncns

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,10 +1,20 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <wctype.h>
+#include <stddef.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
+void print_all(const char *const format, ...);
 int _putchar(char c);
 
 #endif /* VARIADIC_FUNCTIONS_H */
