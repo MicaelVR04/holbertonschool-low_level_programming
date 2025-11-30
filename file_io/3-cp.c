c710664d7ce19005024c2bc6ea7a661658925162
Micael Velez <mvrod2314@gmail.com>
2025-11-30 04:11:22 +0000

    cp0

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,26 +2,109 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define BUFFER_SIZE 1024
 
 /**
+ * open_file_from - opens file_from for reading
+ * @filename: file to open
+ *
+ * Return: file descriptor
+ * Exit: 98 on error
+ */
+int open_file_from(char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * open_file_to - opens file_to for writing
+ * @filename: file to open/create
+ *
+ * Return: file descriptor
+ * Exit: 99 on error
+ */
+int open_file_to(char *filename)
+{
+	int fd;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+		exit(99);
+	}
+	return (fd);
+}
+
+/**
+ * close_fd - closes a file descriptor
+ * @fd: file descriptor
+ *
+ * Exit: 100 on error
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * copy_file - copies content from fd_from to fd_to
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ *
+ * Exit: 98 if read fails, 99 if write fails
+ */
+void copy_file(int fd_from, int fd_to)
+{
+	ssize_t rcount, wcount;
+	char buffer[BUFFER_SIZE];
+
+	while ((rcount = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		wcount = write(fd_to, buffer, rcount);
+		if (wcount != rcount)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to destination file\n");
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (rcount == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from source file\n");
+		close_fd(fd_from);
+		close_fd(fd_to);
+		exit(98);
+	}
+}
+
+/**
  * main - copies the content of one file to another
- * @argc: number of arguments
+ * @argc: argument count
  * @argv: argument vector
  *
  * Return: 0 on success
- * Description: Exits with code on error:
- *   97 -> incorrect argument count
- *   98 -> cannot read from file_from
- *   99 -> cannot write to file_to
- *   100 -> cannot close file descriptor
+ * Exit: 97 if incorrect number of arguments
  */
 int main(int argc, char *argv[])
 {
 	int fd_from, fd_to;
-	ssize_t rcount, wcount;
-	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
 	{
@@ -29,53 +112,11 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
-
-	while ((rcount = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		wcount = write(fd_to, buffer, rcount);
-		if (wcount != rcount)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	}
-
-	if (rcount == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		exit(98);
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
-		exit(100);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	fd_from = open_file_from(argv[1]);
+	fd_to = open_file_to(argv[2]);
+	copy_file(fd_from, fd_to);
+	close_fd(fd_from);
+	close_fd(fd_to);
 
 	return (0);
 }
