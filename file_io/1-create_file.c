05d74f3818a21b6be1d0ffa473706e1a51181af3
Micael Velez <mvrod2314@gmail.com>
2025-11-30 04:00:19 +0000

    cr8FL

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* Create or truncate, write-only, permissions rw-r--r-- */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
